8d7ede589fb8e387efd2c605ab390495095285a2
rohitchopra6611 <rohit.chopra@altruistindia.com>
2024-04-10 10:26:46 +0530

    commited by rohit on 10-4-2024 at 10:26 am

--- src/test/java/com/altruist/ServiceTest.java
+++ src/test/java/com/altruist/ServiceTest.java
@@ -2,24 +2,20 @@ package com.altruist;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.altruist.user.User;
 import com.altruist.user.UserRepository;
